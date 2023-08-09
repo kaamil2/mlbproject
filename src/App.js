f945b5ec7022d96411affde44c082e97e38a3141
kaamil2 <kaamil22thobani@gmail.com>
2023-08-08 22:06:58 -0400

    moving to git

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import axios, {Axios} from "axios";
+
+/*
 import React, {useRef} from "react";
-import {useEffect, useState} from "react";
+*/
 
 //TODO: use the api i have created to project the png of the graph
 
@@ -10,7 +10,7 @@ import {useEffect, useState} from "react";
 
 function App() {
 
-    const [images, setImages] = useState([]);
+    /*const [images, setImages] = useState([]);
 
     const inputRef = useRef(null);
 
@@ -19,29 +19,26 @@ function App() {
     useEffect(() => {
 
         axios.get('https://kaamil2.github.io', {
-            /*params: {
+            /!*params: {
                 o1: 1,
                 b1: 10,
                 t1: "4Yr",
                 p1: "Bat",
                 a5: false
-            }*/
+            }*!/
         })
             .then((res) => {
                 //setImages(res.data);
                 console.log(res.data);
                 // handle success
             })
-    }, []);
+    }, []);*/
 
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Hello This will be the MLB place </h1>
                 <h2>Here is the graph</h2>
-                <img src={images} className="App-logo" alt="logo" />
-
-
 
                 {/*<img src={logo} className="App-logo" alt="logo" />
                 <p>
