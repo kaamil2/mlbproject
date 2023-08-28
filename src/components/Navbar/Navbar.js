fd20c530bc629797bf4f6e5c801e2390dc1eb193
kaamil2 <kaamil22thobani@gmail.com>
2023-08-28 00:00:32 -0500

    fixing the path for the image

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,12 +8,14 @@ class Navbar extends Component {
     handleClick = () => {
         this.setState({ clicked: !this.state.clicked })
     }
-
+// cannot have a absolute path here must be relative by adding the file to the public folder in src the below
+// code will now work it wouldve worked fro you before but when using github you need be cognizent of the path and that
+// it is relative to the public folder and not your local machines folder
     render() {
         return(
             <nav className='NavbarItems'>
                 <div className='navbar-logo'>
-                    <img style={{ width: 70, height: 70 }} src={require('/Users/vamshipagidi/Development/ReactProjects/mlbproject/src/components/Navbar/images/logo.jpg')} alt='logo'></img>
+                    <img style={{ width: 70, height: 70 }} src={require('./images/logo.jpg')} alt='logo'></img>
                 </div>
                 <text className='navbar-title'>Competing Risks of MLB Draft Data</text>
                 <div className='menu-icon' onClick={this.handleClick}>
